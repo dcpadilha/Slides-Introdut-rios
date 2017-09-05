ddbabbf1f2b204bb197f733c0cd19cb82c9119d6
Diogo Padilha <dcpadilha31@gmail.com>
2017-09-04 21:00:49 -0300

    Add files via upload

---
+++ MyApplication/app/src/main/java/com/example/diogo/myapplication/Main2.java
@@ -0,0 +1,18 @@
+package com.example.diogo.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by diogo on 04/09/2017.
+ */
+
+public class Main2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+}
